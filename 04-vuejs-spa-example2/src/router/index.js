43845b74084e73f058de3c040f974cb7edffc728
Your Name <you@example.com>
2020-10-30 11:14:32 +0900

    update new file

--- 04-vuejs-spa-example2/src/router/index.js
+++ 04-vuejs-spa-example2/src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import NotFound from '@components/NotFound'
+import PageA from '@/components/PageA'
+import PageB from '@/components/PageB'
 
 Vue.use(Router)
 
-export default new Router({
+export default new Router({ 
+  mode: 'history', // https://router.vuejs.org/guide/essentials/history-mode.html
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    { path: '/', component: Login },
+    { path: '/Login', name: 'Login', query: { next: '' }, component: Login },
+    { path: '/PageA', name: 'PageA', query: { auth: '' }, component: PageA },
+    { path: '/PageB', name: 'PageB', query: { auth: '' }, component: PageB },
+    { path: '*', component: NotFound }
   ]
 })
